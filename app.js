a9fe5675f8f19fc106ca6be5603618a85a299764
Olivia Tooker <otooker@gmail.com>
2023-04-28 16:22:15 -0400

    got the fox html to show on page

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 // bare min needs: text box, word counter, fox image api FUNCTIONAL PROGRAMMING keep it cleeean please
 
-console.log('Once upon a midnight dreary'); 
+// so in the orinal written kitten, they are calling their functions in the HTML so that way it doesn't get crazy messy in the js page. that being said, that's not good practice and it hurts my head rn 
 
+const currentWordCount = 0;
+const wordsToEarnReward = 100;
+const foxesEarned = 0;
+const foxesShown = 0;
+
+
+console.log('Once upon a midnight dreary'); 
 const showCount = document.querySelector('#show-count');
 //console.log(showCount) 
 
 //the actual wordcounter, it ensures to count on the next line! woohoo! 
 function countWords(string) {
-    let currentWords = 0
+    let currentWords = 0;
     let array = string.split(/\s/);
     array = array.filter(word => word.match(/[A-Za-z0-9]/));
     currentWords = array.filter(word => word !== '').length;
@@ -18,12 +25,7 @@ function countWords(string) {
 //console.log(countWords('This is a bunch of words equalling eight.')) //expecting 8
 //console.log(countWords('Test two.')) //expecting 2
  
-
-//this is supposed to actively listen to the text area to give a us a string to count -- a lot of stuff in the function for event listener because the whole premise of this application depends on it 
-//textArea.value.split(/\s/).filter(word => word.match(/\w/)).length  --- this was helper code
 function showWordCount(){
-    //const userTextInput = document.querySelector('#text-area-box').value;  // moved to event listenr
-    //console.log(userTextInput);
     document.querySelector('#text-area-box').addEventListener("input", () => {
         const userTextInput = document.querySelector('#text-area-box').value;
         const currentWordCount = countWords(userTextInput);
@@ -31,23 +33,12 @@ function showWordCount(){
     });
 }
 showWordCount();
-//showCount.innerHTML = countWords(userTextInput);
-
-// TODO: function for storing highestWordCount 
 
 
 // fetching API image, 123 foxes available 
 const rewardContainer = document.querySelector("#reward-container");
 const baseURL ='https://randomfox.ca/images/';
 
-//get the fox data -- which is really just the image but we gotta fetch somewhere
-async function getFox(number) {
-    return fetch(`${baseURL}${number}.jpg`)
-    //.then(result => result.json()); //not sure if i need this since I'm just pulling one image
-}
-
-
-//console.log(getFox(2)) should bring you to an image of a smiling fox in the woods by some water
 
 // this is a really simple API that literally just gets a photo at a number 1-123. If you're feeling fun and frisky after making this all work, go ahead and try to do the other idea (spooky jokes or riddles) for fun
 const getRandomFoxNumber = () => {
@@ -55,49 +46,68 @@ const getRandomFoxNumber = () => {
     return randomNumber;
 }
 
-console.log(getRandomFoxNumber()) //it's random! so as long as it's a num between 1-123, you're in a good pplace
+//get the fox data -- which is really just the image but we gotta fetch somewhere
+async function getFox(number) {
+    let gottenFox = await fetch(`${baseURL}${number}.jpg`)
+    return gottenFox;
+    //.then(result => result.json()); //not sure if i need this since I'm just pulling one image
+}
 
-// render the fox image on the page
+//console.log(getFox(2)) //should bring you to an image of a smiling fox in the woods by some water
 
-function renderFoxOnWebpage(fox){
+
+// render the fox image on the page, need await async for the promise?
+// HEY YOU figure out where the rewardContainer comes into play 
+
+async function renderFoxOnWebpage(){
+    let imageSource = await getFox(getRandomFoxNumber());
     return `
     <h2>A wild fox appears! Good job writing 100 words ^_^</h2>
-    <h3><img src="${getFox(getRandomFoxNumber())}"></h3>
+    <h3><img src="${imageSource}"></h3>
     `
-}
+    }
+//console.log(renderFoxOnWebpage());
 
 
-//storing highest number function?????? needed????
-
-
-function getRewardedWithFox(){
+async function getRewardedWithFox(){
     
+    //renderFoxOnWebpage()
+    rewardContainer.innerHTML = await renderFoxOnWebpage();
 }
+getRewardedWithFox();
+//showCount.innerHTML = countWords(userTextInput);
+// TODO: function for storing highestWordCount 
 
 
 
-//console.log(renderFoxOnWebpage()); //not sure if this works yet, test may
+//TODO: function for displaying API at intervals of 100, onchange event listener might help
 
-
-//TODO: function for displaying API at intervals of 100 
-
-function main(){
-    renderFoxOnWebpage();
-}
 
 
 
 /* SPACE FOR DRAFT CODE 
 
+//this should help us not lose text in between
+function restoreText() {
+	if (typeof localStorage != "undefined") { 
+		$("#text-area-box").attr("value", localStorage.text);
+	}
+}
 
 
 
 
+function countedWordsFromInput() {
+    document.querySelector('#text-area-box').addEventListener("input", () => {
+        const userTextInput = document.querySelector('#text-area-box').value;
+        const currentWordCount = countWords(userTextInput);
+        return currentWordCount;
+}
 
 
 
-
+function main(){
+    renderFoxOnWebpage();
+}
 
 */
-
-
