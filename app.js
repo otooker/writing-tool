12b01710838d3f91330d6ba396915d4e0de8f1fc
Olivia Tooker <otooker@gmail.com>
2023-05-04 00:34:49 -0400

    cleaned up comments

--- app.js
+++ app.js
@@ -9,17 +9,87 @@ let foxesShown = 0;
 console.log('Once upon a midnight dreary'); 
 const showCount = document.querySelector('#show-count');
 
-
-//TODO USER INPUT WORD GOAL SECTION
-//Event listener to make the user create the wordgoal
-//you may have to change the logic behind get reward, so that way you can change 
-
+//Event listener to let the user choose the wordgoal
 document.getElementById('set-word-goal').addEventListener("change", () => {
     let userInputWordGoal = document.getElementById('set-word-goal').value;
     console.log(userInputWordGoal);
     wordsToEarnReward = userInputWordGoal;
 })
 
+
+//the actual wordcounter, it ensures to count on the next line! woohoo! 
+function countWords(string) {
+    let currentWords = 0;
+    let array = string.split(/\s/);
+    array = array.filter(word => word.match(/[A-Za-z0-9]/));
+    currentWords = array.filter(word => word !== '').length;
+    return currentWords;
+}
+
+// tests for countWords()
+//console.log(countWords('This is a bunch of words equalling eight.')) //expecting 8
+//console.log(countWords('Test two.')) //expecting 2
+
+
+
+//this event listener acts as our main function: it allows the current word count to be accessible to the rest of the code, which is important because the whole point of this tool hinges on this
+document.querySelector('#text-area-box').addEventListener("input", () => {
+    const userTextInput = document.querySelector('#text-area-box').value;
+    currentWordCount = countWords(userTextInput);
+    showWordCount(currentWordCount);
+    checkRewardEligibility();
+});
+
+
+
+//Displays wordcount on web page
+function showWordCount(currentWordCount){
+    document.getElementById('show-count').innerText = currentWordCount; 
+}
+
+//fetching from Random Fox API
+async function getFoxRemotely() {
+    const response = await fetch("https://randomfox.ca/floof/");
+    const foxData = await response.json();
+    return foxData;
+}
+
+//this function display the fox image and HTML onto the website.
+const rewardContainer = document.querySelector("#reward-container");
+
+async function rewardUserWithFox(){
+    const gottenFoxData = getFoxRemotely();
+    gottenFoxData.then((data) => {  
+        rewardContainer.innerHTML = `
+           <h2>A wild fox appears!</h2>
+           <h3><img id="fox-image" src="${data.image}"></h3>
+           <h3>Good job meeting your word goal.</h3>
+           <h3>Do it again for another fox ^_^</h3>
+          `
+    })
+}
+
+//do you deserve the fox?????? this function will tell 
+function checkRewardEligibility(){
+    const milestonesPassed = Math.floor(currentWordCount/wordsToEarnReward);
+    if(milestonesPassed > foxesShown) {
+        foxesEarned = milestonesPassed;
+    }
+    if (foxesEarned > foxesShown){
+        rewardUserWithFox();
+        foxesShown++;
+}
+}
+
+
+/* SPACE FOR DRAFT CODE 
+
+
+
+function setRewardParameter(userInputWordGoal) {
+    let wordsToEarnReward = userInputWordGoal;
+    console.log(wordsToEarnReward);
+}
 //setRewardParameter(userInputWordGoal);
 
 //let wordsToEarnReward = document.getElementById('set-word-goal').value;
@@ -44,93 +114,3 @@ userInputWordGoal.addEventListener("onchange", (num) =>
 
 */
 
-/*
- */
-
-//// if STUFF MET, then activate the reward function above OKAY YEAH 
-//const wordGoal = 100;
-
-//the actual wordcounter, it ensures to count on the next line! woohoo! 
-function countWords(string) {
-    let currentWords = 0;
-    let array = string.split(/\s/);
-    array = array.filter(word => word.match(/[A-Za-z0-9]/));
-    currentWords = array.filter(word => word !== '').length;
-    return currentWords;
-}
-
-// tests for countWords()
-//console.log(countWords('This is a bunch of words equalling eight.')) //expecting 8
-//console.log(countWords('Test two.')) //expecting 2
-
-
-
-//this acts as our main function: it allows the current word count to be accessible to the rest of the code, which is important because the whole point of this tool hinges on this
-
-document.querySelector('#text-area-box').addEventListener("input", () => {
-    const userTextInput = document.querySelector('#text-area-box').value;
-    currentWordCount = countWords(userTextInput);
-    showWordCount(currentWordCount);
-    checkRewardEligibility();
-});
-
-//Displays wordcount on web page
-function showWordCount(currentWordCount){
-    document.getElementById('show-count').innerText = currentWordCount; 
-}
-//showWordCount();
-
-
-const rewardContainer = document.querySelector("#reward-container");
-
-//fetching from Random Fox API
-async function getFoxRemotely() {
-    const response = await fetch("https://randomfox.ca/floof/");
-    const foxData = await response.json();
-    return foxData;
-}
-
-//this function display the fox image and HTML onto the website.
-async function rewardUserWithFox(){
-    const gottenFoxData = getFoxRemotely();
-    gottenFoxData.then((data) => {  
-        rewardContainer.innerHTML = `
-           <h2>A wild fox appears!</h2>
-           <h3><img id="fox-image" src="${data.image}"></h3>
-           <h3>Good job meeting your word goal.</h3>
-           <h3>Do it again for another fox ^_^</h3>
-          `
-    })
-}
-
-
-//do you deserve the fox?????? this function will tell 
-function checkRewardEligibility(){
-    const milestonesPassed = Math.floor(currentWordCount/wordsToEarnReward);
-    if(milestonesPassed > foxesShown) {
-        foxesEarned = milestonesPassed;
-    }
-    if (foxesEarned > foxesShown){
-        rewardUserWithFox();
-        foxesShown++;
-}
-}
-
-
-/* SPACE FOR DRAFT CODE 
-
-
-//this should help us not lose text in between
-function restoreText() {
-	if (typeof localStorage != "undefined") { 
-		$("#text-area-box").attr("value", localStorage.text);
-	}
-}
-
-
-function setRewardParameter(userInputWordGoal) {
-    let wordsToEarnReward = userInputWordGoal;
-    console.log(wordsToEarnReward);
-}
-
-*/
